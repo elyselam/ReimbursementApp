8b635a048006f7a279cfa7c5c249a2cf132c7bd1
Elyse Lam <elyse.p.lam@gmail.com>
2019-10-05 22:40:08 -0400

    finished Navigator and Actions

--- proj1/src/main/java/com/wu/app/controller/LoginActions.java
+++ proj1/src/main/java/com/wu/app/controller/LoginActions.java
@@ -1,3 +1,44 @@
+//package com.wu.app.controller;
+//
+//import com.wu.app.dao.UserRepository;
+//import com.wu.app.dao.data.JDBCUserDao;
+//import com.wu.app.model.User;
+//import com.wu.app.services.LoginService;
+//import org.apache.logging.log4j.Logger;
+//
+//
+//import javax.servlet.http.HttpServletRequest;
+//
+//public class LoginActions {
+////    static Logger logger = Logger.getLogger(LoginActions.class);
+//
+//
+//    public static String Login(HttpServletRequest request) {
+//        LoginService loginServ = (LoginService) request.getServletContext().getAttribute( "loginServiceServ");
+//
+//        String email = request.getParameter("email");
+//        String password = request.getParameter("password");
+//        //if person exists & password is correct
+//        //retrieve user from DB set it equal to a user OBJ
+//        User u = new User(0, "Hotmoves", "Ghandi", email, password, false);
+//        //findEmployeeByID/Username returns user
+//        User fullUser = loginServ.doService(u);
+//        //set the user as a session attribute
+//        if (fullUser.getEmployeeID() == -2 || fullUser == null) {
+//            return "/Login.html";
+//        } else if (fullUser.isManager()) {
+//            request.getSession().setAttribute("user", fullUser);
+//            return "/Manager.html";
+//        } else {
+//            request.getSession().setAttribute("user", fullUser);
+//            return "/Employee.html";
+//        }
+//
+//
+//    }
+//}
+
+
 package com.wu.app.controller;
 
 import com.wu.app.dao.UserRepository;
@@ -13,7 +54,7 @@ public class LoginActions {
 //    static Logger logger = Logger.getLogger(LoginActions.class);
 
 
-    public static String Login(HttpServletRequest request) {
+    public static String login(HttpServletRequest request) {
         LoginService loginServ = (LoginService) request.getServletContext().getAttribute( "loginServiceServ");
 
         String email = request.getParameter("email");
@@ -37,4 +78,3 @@ public class LoginActions {
 
     }
 }
-
--- proj1/src/main/java/com/wu/app/controller/Navigator.java
+++ proj1/src/main/java/com/wu/app/controller/Navigator.java
@@ -10,23 +10,23 @@ public class Navigator {
     public static String navigate(HttpServletRequest req, HttpServletResponse res){
         switch(req.getRequestURI()) {
             case "/Login.do":
-                return LoginActions.Login(req);
-//            case "/submitTicket.do":
-//                return EmployeeActions.submitTicket(req);
-//            case "/updateInfo.do":
-//                return EmployeeActions.updateInfo(req,res);
-//            case "/empViewTickets.do":
-//                return EmployeeActions.viewTickets(req, res);
-//            case "/managerViewAll.do":
-//                return ManagerActions.managerViewAll(req,res);
-//            case "/managerViewAllPending.do":
-//                return ManagerActions.viewAllPending(req,res);
-//            case "/managerViewAllResolved.do":
-//                return ManagerActions.viewAllResolved(req, res);
-//            case "/managerExamineEmployee.do":
-//                return ManagerActions.examineEmployee(req, res);
-//            case "/managerRegisterEmployee.do":
-//                return ManagerActions.RegisterEmployee(req);
+                return LoginActions.login(req);
+            case "/submitTicket.do":
+                return EmployeeActions.submitTicket(req);
+            case "/updateInfo.do":
+                return EmployeeActions.updateInfo(req,res);
+            case "/empViewTickets.do":
+                return EmployeeActions.viewTickets(req, res);
+            case "/managerViewAll.do":
+                return ManagerActions.managerViewAllEmployee(req,res);
+            case "/managerViewAllPending.do":
+                return ManagerActions.viewAllPending(req,res);
+            case "/managerViewAllResolved.do":
+                return ManagerActions.viewAllResolved(req, res);
+            case "/managerExamineEmployee.do":
+                return ManagerActions.examineEmployee(req, res);
+            case "/managerRegisterEmployee.do":
+                return ManagerActions.RegisterEmployee(req);
             default:
                 return "/Login.html";
         }
